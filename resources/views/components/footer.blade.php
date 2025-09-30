a8eee2de7dda2e73313da210151944446dd8ea9e
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-09-30 16:37:43 -0300

    Fix styling issue with footer logo

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div class="grid lg:grid-cols-6 gap-8 place-items-center lg:place-items-start text-center lg:text-left">
             <div class="lg:col-span-2">
-                <div class="flex flex-col items-center lg:items-start space-y-3 mb-6">
+                <div class="flex md:flex-row md:items-center flex-col items-center md:space-x-3 space-y-3 mb-6">
                     <x-logo />
                     <span
                         class="text-xl font-bold bg-gradient-to-r from-amber-400 to-yellow-500 bg-clip-text text-transparent">
