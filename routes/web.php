d8690724d752499eb5be49a3f73cb3edb0f3b511
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-10-21 17:04:28 -0300

    Protect routes using guest and auth middleware

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,13 @@ Route::get('/planos', function () {
     return view('pages.planos');
 })->name('planos');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/create-account', [LoginController::class, 'index'])->name('create-account');
+Route::middleware('guest')->group(function() {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+    Route::get('/create-account', [LoginController::class, 'index'])->name('create-account');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
